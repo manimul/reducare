48ad3cfa498f5274ed189790fa5eb3f0290f1e59
Mark Mulvaney <mark@bambwa.com>
2022-03-15 13:59:55 +0100

    updated to enable internationalization

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,15 +1,3 @@
-import S from '@sanity/desk-tool/structure-builder'
+import * as Structure from '@sanity/document-internationalization/lib/structure'
 
-// We filter document types defined in structure to prevent
-// them from being listed twice
-const hiddenDocTypes = (listItem) => !['page', 'route', 'site-config'].includes(listItem.getId())
-
-export default () =>
-  S.list()
-    .title('Site')
-    .items([
-      S.documentListItem().id('global-config').schemaType('site-config').title('Site config'),
-      S.documentTypeListItem('page').title('Pages'),
-      S.documentTypeListItem('route').title('Routes'),
-      ...S.documentTypeListItems().filter(hiddenDocTypes),
-    ])
+export default Structure.default
--- studio/schemas/documents/page.js
+++ studio/schemas/documents/page.js
@@ -4,18 +4,19 @@ export default {
   name: 'page',
   type: 'document',
   title: 'Page',
+  i18n: true,
   icon: MasterDetailIcon,
   fieldsets: [
     {
       title: 'SEO & metadata',
-      name: 'metadata',
-    },
+      name: 'metadata'
+    }
   ],
   fields: [
     {
       name: 'title',
       type: 'string',
-      title: 'Title',
+      title: 'Title'
     },
     {
       name: 'content',
@@ -25,29 +26,29 @@ export default {
         { type: 'hero' },
         { type: 'imageSection' },
         { type: 'mailchimp' },
-        { type: 'textSection' },
-      ],
+        { type: 'textSection' }
+      ]
     },
     {
       name: 'description',
       type: 'text',
       title: 'Description',
       description: 'This description populates meta-tags on the webpage',
-      fieldset: 'metadata',
+      fieldset: 'metadata'
     },
     {
       name: 'openGraphImage',
       type: 'image',
       title: 'Open Graph Image',
       description: 'Image for sharing previews on Facebook, Twitter etc.',
-      fieldset: 'metadata',
-    },
+      fieldset: 'metadata'
+    }
   ],
 
   preview: {
     select: {
       title: 'title',
-      media: 'openGraphImage',
-    },
-  },
+      media: 'openGraphImage'
+    }
+  }
 }
