1b9af72fd1fe47350679a5101172f1d7e33a16de
Mark Mulvaney <mark@bambwa.com>
2022-04-01 16:49:47 +0200

    fixed issue

--- web/components/sections/ImageSection.js
+++ web/components/sections/ImageSection.js
@@ -17,26 +17,6 @@ function ImageSection(props) {
 
   return (
     <div className={styles.root}>
-      {/*comment here
-      
-      <figure className={styles.content}>
-        <img
-          src={builder.image(image).auto('format').width(2000).url()}
-          className={styles.image}
-          alt={heading}
-        />
-        <figcaption>
-          <div className={styles.caption}>
-            <div className={styles.captionBox}>
-              <div className={styles.label}>{label}</div>
-              <h2 className={styles.title}>{heading}</h2>
-              {text && <SimpleBlockContent blocks={text} />}
-              {cta && cta.route && <Cta {...cta} />}
-            </div>
-          </div>
-        </figcaption>
-      </figure>
-*/}
       <section className=" container mx-auto flex flex-row">
         <div className="w-1/2 ">
           <img
